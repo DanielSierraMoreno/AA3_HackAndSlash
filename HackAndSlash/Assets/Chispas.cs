cd263cd9f68298b3845bb017e32321b8b4e1156e
Daniel Sierra Moreno <daniel.sierra@enti.cat>
2023-10-31 13:43:30 +0100

    a

---
+++ HackAndSlash/Assets/Chispas.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chispas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Enemy"))
+        {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Enemy"))
+        {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+
+        }
+    }
+}
